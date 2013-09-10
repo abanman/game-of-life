8558819f9246e62b44a092c5374a4463a7fba2d1
abanman <abanman@macalester.edu>
2013-09-10 11:08:55 -0500

    moved HighLifeTest to Test folder

--- test/edu/macalester/comp124/life/HighLifeTest.java
+++ test/edu/macalester/comp124/life/HighLifeTest.java
@@ -3,7 +3,8 @@ package edu.macalester.comp124.life;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created with IntelliJ IDEA.
